8b566171d45242ae387b1226559579a76d1a506c
Yogesh Bhatia <yogeshbhatia677@gmail.com>
2021-12-25 11:21:00 +0530

    Update tip_calculator.py

--- tip_calculator.py
+++ tip_calculator.py
@@ -7,4 +7,5 @@ tip=int(tip)
 people=input("How many people to split the bill? ")
 people=int(people)
 total_bill=(bill+(bill*tip*0.01))/people
-print(round(total_bill,2))
+total_bill="{:.2f}".format(round(total_bill,2))
+print(f"Each person will pay {total_bill}")
